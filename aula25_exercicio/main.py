7baccd4302672f75a01883651bcf998e8aa8003c
Loadingzs <maykon07dias@gmail.com>
2025-10-02 11:13:42 -0300

    Primeira versão

--- aula25_exercicio/main.py
+++ aula25_exercicio/main.py
@@ -20,29 +20,34 @@ def get_db_connection():
         database="aula25"
     )
 
+
 @app_web.get("/", response_class=HTMLResponse)
 def login_form(request: Request):
     return paginas.TemplateResponse("login.html", {"request": request})
+
 
 @app_web.post("/login")
 def login(
     Request: Request,
     response: Response,
     nome: str = Form(...),
-    senha: str = Form(...),
-):
+    senha: str = Form(...),):
+
     conn = get_db_connection()
     cursor = conn.cursor()
-    hash_password = hashlib.md5(senha.encode()).hexdigest()
+
+    hash_md5 = hashlib.md5(senha.encode()).hexdigest()
     cursor.execute("SELECT password FROM tb_administracao WHERE login = %s", (nome,))
-    result = cursor.fetchone()
+    resultado = cursor.fetchone()
+
     cursor.close()
     conn.close()
-    if result and result[0] == hash_password:
+    if resultado and resultado[0] == hash_md5:
         Request.session['user'] = nome
         return RedirectResponse("/sucesso", status_code=HTTP_302_FOUND)
     else:
         return RedirectResponse("/falha", status_code=HTTP_302_FOUND)
+
 
 @app_web.get("/sucesso", response_class=HTMLResponse)
 def success(request: Request):
@@ -50,6 +55,7 @@ def success(request: Request):
     if not user:
         return RedirectResponse("/", status_code=HTTP_302_FOUND)
     return paginas.TemplateResponse("sucesso.html", {"request": request, "user": user})
+
 
 @app_web.get("/falha", response_class=HTMLResponse)
 def fail(request: Request):
