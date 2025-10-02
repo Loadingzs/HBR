6c681005bbf1e3a259d4380eaf6da9ad6db8df1d
Loadingzs <maykon07dias@gmail.com>
2025-10-02 09:49:00 -0300

    .

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Form
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
+
+app_web = FastAPI()
+paginas = Jinja2Templates(directory='HBR/paginas')
+
+#acrescentando um comentário para atualizar o GitHub
+
+@app_web.get('/')
+async def index(request: Request):
+    context = {
+        "request": request,
+        "resultado": 0
+    }
+    return paginas.TemplateResponse('calculadora.html', context)
+
